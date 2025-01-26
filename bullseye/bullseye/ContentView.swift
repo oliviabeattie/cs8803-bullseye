628159f5353047deb301c8a34a27fd72c536a55c
oliviabeattie <oliviabeattie@gatech.edu>
2025-01-26 18:30:10 -0500

    created application

---
+++ bullseye/bullseye/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bullseye
+//
+//  Created by Olivia Beattie on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bullseye/bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Olivia Beattie on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
