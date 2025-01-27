24942b35b595b94c424abbdf359d21dffcdb3784
oliviabeattie <oliviabeattie@gatech.edu>
2025-01-26 22:34:47 -0500

    game functions finished

--- bullseye/bullseye/ContentView.swift
+++ bullseye/bullseye/ContentView.swift
@@ -9,27 +9,123 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertVisible: Bool = false
+    @State var alertVisible = false
+    @State var sliderValue = 50.0
+    
+    @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
+    
+    func calculateScore() -> Int {
+        let difference = abs(target - Int(sliderValue.rounded()))
+        var points = 100 - difference
+        
+        if (difference == 0) {
+            points += 100
+        } else if (difference == 1) {
+            points += 50
+        }
+            
+        return points
+    }
+    
+    func alertTitle() -> String {
+        let difference = abs(target - Int(sliderValue.rounded()))
+        let title: String
+        
+        if (difference == 0) {
+            title = "Perfect!"
+        } else if (difference < 5) {
+            title = "You almost had it!"
+        } else if (difference <= 10) {
+            title = "Not bad."
+        } else {
+            title = "Are you even trying?"
+        }
+        
+        return title
+    }
+    
+    func startOver () {
+        score = 0
+        round = 1
+        sliderValue = 50.0
+        target = Int.random(in: 1...100)
+    }
 
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            Spacer()
             
+            // target
+            HStack {
+                Text("Put the bullseye as close as you can to: ")
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                Text("\(target)")
+            }
+            Spacer()
+            
+            // slider
+            HStack {
+                Text("1")
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                Slider(value: $sliderValue, in:1...100)
+                Text("100")
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+                    .font(Font.custom("Arial Rounded MT Bold", size: 18))
+            }
+            Spacer()
+            
+            // button
             Button(action: {
-                print("button pressed")
                 self.alertVisible = true }) {
                 Text("HIT ME")
             }.alert(isPresented: $alertVisible) { () ->
                 Alert in
-                return Alert(title: Text("Hello there!"),
-                             message: Text("pop-up"),
-                             dismissButton: .default(Text("Awesome!")))
+                return Alert(title: Text(alertTitle()),
+                             message: Text("The slider value is \(Int(sliderValue.rounded())).\n" +
+                                           "You scored \(calculateScore()) this round!"),
+                             dismissButton: .default(Text("Awesome!")) {
+                                            score += calculateScore()
+                                            round += 1
+                                            target = Int.random(in: 1...100) })
+            }
+            Spacer()
+            // score
+            HStack {
+                // restart button
+                Button(action: {
+                    startOver() }) {
+                    Text("Start Over")
+                }
+                Spacer()
+                
+                // score
+                Text("Score:")
+                Text("\(score)")
+                Spacer()
+                
+                // round
+                Text("Round:")
+                Text("\(round)")
+                Spacer()
+                
+                // info button
+                Button(action: {}) {
+                    Text("Info")
+                }
             }
         }
-        .padding()
+        .padding(.bottom, 20)
+        .background(Image("Background"), alignment: .center)
     }
 }
 
-#Preview {
+#Preview(traits: .fixedLayout(width: 896, height: 414)) {
     ContentView()
 }
