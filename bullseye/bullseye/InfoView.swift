9586014e5e08d2cc5c9ce3782ab716d698335c09
oliviabeattie <oliviabeattie@gatech.edu>
2025-01-26 23:00:53 -0500

    created info page

---
+++ bullseye/bullseye/InfoView.swift
@@ -0,0 +1,19 @@
+//
+//  InfoView.swift
+//  bullseye
+//
+//  Created by Olivia Beattie on 1/26/25.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text("this is the info page")
+    }
+}
+
+
+#Preview(traits: .fixedLayout(width: 896, height: 414)) {
+    InfoView()
+}
